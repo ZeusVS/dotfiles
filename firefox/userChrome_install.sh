a12571a16481158753f445dfee5b4557a17674df
ZeusVS <zinovansummeren@gmail.com>
2024-06-06 20:51:44 +0200

    removed firefox dotfiles

--- firefox/userChrome_install.sh
+++
@@ -1,8 +0,0 @@
-# First we create the chrome folder, then make a symbolic link there
-# We loop over the found wildcards to that we put the userChrome in all default directories
-profile=~/.mozilla/firefox/*default
-for i in $profile; do
-rm -rf "$i/chrome"
-mkdir $i/chrome
-ln -s "$HOME/dotfiles/firefox/userChrome.css" "$i/chrome/"
-done
